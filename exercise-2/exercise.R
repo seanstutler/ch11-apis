fc4cb9ffd734fb93e10bdf2e3f12f811a128ddaf
Sean Yang <seanunbelievably@gmail.com>
2018-04-23 16:06:55 -0700

    finished

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,10 +6,10 @@ library(jsonlite)
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.R")
 
 # Create a variable `movie.name` that is the name of a movie of your choice.
-
+movie.name <- "Ready Player One"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -19,26 +19,34 @@ library(jsonlite)
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie.name` variable as the search query!
-
-
+base_url <- "https://api.nytimes.com/svc/movies/v2"
+resource <- "/reviews/search.json"
+query_params <- list("api-key" = nty_apikey, query = movie.name)
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(paste0(base_url, resource), query = query_params)
+body <- content(response, "text")
+data <- fromJSON(body)
 
 # What kind of data structure did this produce? A data frame? A list?
+typeof(data)
 
-
-# Manually inspect the returned data and identify the content of interest 
+# Manually inspect the returned data and identify the content of interest
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+View(data)
+names(data$results)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(data$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
-
-# Create a list of the three pieces of information from above. 
+most_recent <- reviews[1, ]
+headline <- most_recent$headline
+short_summary <- most_recent$summary_short
+full_article <- most_recent$link.url
+# Create a list of the three pieces of information from above.
 # Print out the list.
+information <- list(headline, short_summary, full_article)
+print(information)
